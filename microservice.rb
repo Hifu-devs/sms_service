0fd6a52d31318b9942abadf2f05b5129686a6cc4
“Krista <stadler.krista@gmail..com>
2020-07-18 13:17:00 -0600

    Added post route

--- microservice.rb
+++ microservice.rb
@@ -1,5 +1,10 @@
+require 'twilio-ruby'
 require 'sinatra'
 
-get '/' do
-  'Hello World!'
-end 
+post '/alert' do
+  twiml = Twilio::TwiML::MessagingResponse.new do |r|
+    r.message(body: 'Ahoy! Thanks so much for your message.')
+  end
+
+  twiml.to_s
+end
