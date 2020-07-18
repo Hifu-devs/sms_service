df1cba93b8c55789855137021660351d836ecb19
“Krista <stadler.krista@gmail..com>
2020-07-18 13:20:03 -0600

    Test endpoint

--- microservice.rb
+++ microservice.rb
@@ -1,6 +1,10 @@
 require 'twilio-ruby'
 require 'sinatra'
 
+get '/' do
+  puts "Hello World"
+end
+
 post '/alert' do
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
     r.message(body: 'Ahoy! Thanks so much for your message.')
